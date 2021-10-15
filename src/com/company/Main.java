210ed4064a17b0c379573d2ac305809c8d0a4195
Visl <5vladik068@gmail.com>
2021-10-15 21:40:44 +0300

    HomeTask 3.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    byte a = 100;
+    short b = 1243;
+    int c = 422232;
+    long d = 100_000_000_000L;
+    float e = 3782.342f;
+    double f = 765.342_233_343;
+    char g = 'd';
+    char g1 = '\u0453';
+    char g2 = 2341;
+    boolean h = true;
+
+        System.out.println("Значение типа byte " + a);
+        System.out.println("Значение типа short " + b);
+        System.out.println("Значение типа int " + c);
+        System.out.println("Значение типа long " + d);
+        System.out.println("Значение типа float " + e);
+        System.out.println("Значение типа double " + f);
+        System.out.println("Значение типа char " + g + " " + g1 + " " + g2);
+        System.out.println("Значение типа boolean " + h);
+    }
+
+
+
+}
